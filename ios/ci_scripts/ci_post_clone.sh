0c4307edcf95a4110ff43ee4cdc234d7e4abae3c
richtozer <richard.tozer@gmail.com>
2023-12-24 07:02:27 +0800

    chmod

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,4 +1,5 @@
 #!/bin/s
+chmod +x $0
 
 # The default execution directory of this script is the ci_scripts directory.
 cd CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
