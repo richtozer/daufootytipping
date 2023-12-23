ce273b58d52ae6555ff8aa4310e9b157f7712eaa
richtozer <richard.tozer@gmail.com>
2023-12-24 06:36:57 +0800

    fic xcode cloud buid

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,7 +1,35 @@
-#!/bin/sh
+#!/bin/s#!/bin/sh
+
+# The default execution directory of this script is the ci_scripts directory.
+cd $CI_WORKSPACE # change working directory to the root of your cloned repo.
+
+# Install Flutter using git.
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+
+# Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
+flutter precache --ios
+
+# Install Flutter dependencies.
+flutter pub get
+
+# Install CocoaPods using Homebrew.
+HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
+brew install cocoapods
+
+
+cd ios
+pod deintegrate
+# Install CocoaPods dependencies.
+pod update # run `pod install` in the `ios` directory.
+
+flutter build ios --release --no-codesign
+exit 0
 
 #  ci_post_clone.sh
 #  Runner
-#
+# https://stackoverflow.com/questions/74781925/xcode-cloud-unable-to-load-contents-of-file-list-target-support-files-pods-r
+
 #  Created by Richard Tozer on 24/12/2023.
 #  
