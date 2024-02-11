47630955666a57038d1ba4c5037db64cae13eabb
richtozer <richard.tozer@gmail.com>
2024-02-11 10:47:50 +1030

    fix launch experience

--- lib/pages/user_auth/user_auth.dart
+++ lib/pages/user_auth/user_auth.dart
@@ -58,134 +58,134 @@ class _UserAuthPageState extends State<UserAuthPage> {
   @override
   Widget build(BuildContext context) {
     log('UserAuthPage.build()');
-    return ChangeNotifierProvider<TippersViewModel>(
-        create: (_) => TippersViewModel(widget.firebaseService),
-        child: StreamBuilder<User?>(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            // check if the current app version is lower than the value set in
-            // remote config, if so, force the user to update the app
-            return FutureBuilder<bool>(
-              future: isClientVersionOutOfDate(),
-              builder: (context, versionSnapshot) {
-                if (versionSnapshot.connectionState ==
-                    ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (versionSnapshot.hasError) {
-                  return Text('Error: ${versionSnapshot.error}');
-                } else if (versionSnapshot.data == true) {
-                  return const Scaffold(
-                    body: Center(
-                      child: Text(
-                        "This version of the app is no longer supported, please update the app from the app store.",
-                      ),
-                    ),
-                  );
-                }
-                if (!snapshot.hasData) {
-                  return SignInScreen(
-                    providers: [
-                      AppleProvider(),
-                      GoogleProvider(clientId: clientId),
-                      EmailAuthProvider(),
-                    ],
-                    headerBuilder: (context, constraints, shrinkOffset) {
-                      return const Padding(
-                        padding: EdgeInsets.all(20),
-                        child: AspectRatio(
-                          aspectRatio: 1,
-                          child: Image(
-                            image: AssetImage('assets/icon/AppIcon.png'),
-                          ),
-                        ),
-                      );
-                    },
-                    subtitleBuilder: (context, action) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: action == AuthAction.signIn
-                            ? const Text(
-                                'Welcome to DAU Footy Tipping, please sign in!')
-                            : const Text(
-                                'Welcome to DAU Footy Tipping, please sign up!'),
-                      );
-                    },
-                    footerBuilder: (context, action) {
-                      return const Padding(
-                        padding: EdgeInsets.only(top: 16),
+    return Scaffold(
+      body: ChangeNotifierProvider<TippersViewModel>(
+          create: (_) => TippersViewModel(widget.firebaseService),
+          child: StreamBuilder<User?>(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              // check if the current app version is lower than the value set in
+              // remote config, if so, force the user to update the app
+              return FutureBuilder<bool>(
+                future: isClientVersionOutOfDate(),
+                builder: (context, versionSnapshot) {
+                  if (versionSnapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (versionSnapshot.hasError) {
+                    return Text('Error: ${versionSnapshot.error}');
+                  } else if (versionSnapshot.data == true) {
+                    return const Center(
                         child: Text(
-                          'By signing in, you ackonwledge you are a paid up member of DAU Footy Tipping.',
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      );
-                    },
-                  );
-                }
-
-                //once we pass signin we have a firebase auth user context
-                User? authenticatedFirebaseUser = snapshot.data;
-                if (authenticatedFirebaseUser == null) {
-                  throw Exception('No user context found');
-                }
-                if (authenticatedFirebaseUser.isAnonymous) {
-                  throw Exception('User is anonymous');
-                }
-                if (authenticatedFirebaseUser.emailVerified == false) {
-                  throw Exception('User email not verified');
-                }
-
-                FirebaseAnalytics.instance.logLogin(
-                    loginMethod:
-                        authenticatedFirebaseUser.providerData[0].providerId);
-
-                //at this point we have a verfied logged on user - as we send them
-                //to the home page, make sure they are represented in the realtime database
-                // as a tipper linked to their firebase auth record,
-                //if not create a Tipper record for them.
-
-                TippersViewModel tippersViewModel =
-                    Provider.of<TippersViewModel>(context, listen: false);
-
-                return FutureBuilder<Tipper>(
-                  future: tippersViewModel.getLinkedTipper(),
-                  builder:
-                      (BuildContext context, AsyncSnapshot<Tipper> snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                          child:
-                              CircularProgressIndicator()); // or your own loading widget
-                    } else if (snapshot.hasError) {
-                      return ProfileScreen(
-                        actions: [
-                          DisplayNameChangedAction((context, oldName, newName) {
-                            // TODO do something with the new name
-                            throw UnimplementedError();
-                          }),
-                        ],
-                        children: [
-                          Container(
-                            color: Colors.red,
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              '${snapshot.error}',
-                              style: const TextStyle(color: Colors.white),
+                      "This version of the app is no longer supported, please update the app from the app store.",
+                    ));
+                  }
+                  if (!snapshot.hasData) {
+                    return SignInScreen(
+                      providers: [
+                        AppleProvider(),
+                        GoogleProvider(clientId: clientId),
+                        EmailAuthProvider(),
+                      ],
+                      headerBuilder: (context, constraints, shrinkOffset) {
+                        return const Padding(
+                          padding: EdgeInsets.all(20),
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: Image(
+                              image: AssetImage('assets/icon/AppIcon.png'),
                             ),
                           ),
-                        ],
-                      );
-                    } else {
-                      Tipper linkedTipper = snapshot.data as Tipper;
-                      return Consumer<TippersViewModel>(
-                        builder: (context, tippersViewModel, child) {
-                          return HomePage(linkedTipper);
-                        },
-                      );
-                    }
-                  },
-                );
-              },
-            );
-          },
-        ));
+                        );
+                      },
+                      subtitleBuilder: (context, action) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: action == AuthAction.signIn
+                              ? const Text(
+                                  'Welcome to DAU Footy Tipping, please sign in!')
+                              : const Text(
+                                  'Welcome to DAU Footy Tipping, please sign up!'),
+                        );
+                      },
+                      footerBuilder: (context, action) {
+                        return const Padding(
+                          padding: EdgeInsets.only(top: 16),
+                          child: Text(
+                            'By signing in, you ackonwledge you are a paid up member of DAU Footy Tipping.',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        );
+                      },
+                    );
+                  }
+
+                  //once we pass signin we have a firebase auth user context
+                  User? authenticatedFirebaseUser = snapshot.data;
+                  if (authenticatedFirebaseUser == null) {
+                    throw Exception('No user context found');
+                  }
+                  if (authenticatedFirebaseUser.isAnonymous) {
+                    throw Exception('User is anonymous');
+                  }
+                  if (authenticatedFirebaseUser.emailVerified == false) {
+                    throw Exception('User email not verified');
+                  }
+
+                  FirebaseAnalytics.instance.logLogin(
+                      loginMethod:
+                          authenticatedFirebaseUser.providerData[0].providerId);
+
+                  //at this point we have a verfied logged on user - as we send them
+                  //to the home page, make sure they are represented in the realtime database
+                  // as a tipper linked to their firebase auth record,
+                  //if not create a Tipper record for them.
+
+                  TippersViewModel tippersViewModel =
+                      Provider.of<TippersViewModel>(context, listen: false);
+
+                  return FutureBuilder<Tipper>(
+                    future: tippersViewModel.getLinkedTipper(),
+                    builder:
+                        (BuildContext context, AsyncSnapshot<Tipper> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                            child:
+                                CircularProgressIndicator()); // or your own loading widget
+                      } else if (snapshot.hasError) {
+                        return ProfileScreen(
+                          actions: [
+                            DisplayNameChangedAction(
+                                (context, oldName, newName) {
+                              // TODO do something with the new name
+                              throw UnimplementedError();
+                            }),
+                          ],
+                          children: [
+                            Container(
+                              color: Colors.red,
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                '${snapshot.error}',
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        );
+                      } else {
+                        Tipper linkedTipper = snapshot.data as Tipper;
+                        return Consumer<TippersViewModel>(
+                          builder: (context, tippersViewModel, child) {
+                            return HomePage(linkedTipper);
+                          },
+                        );
+                      }
+                    },
+                  );
+                },
+              );
+            },
+          )),
+    );
   }
 }
