2335569e8c11387b3c3c9f3b68a50272b9c442d6
richtozer <richard.tozer@gmail.com>
2024-02-23 08:57:11 +1030

    scoring leaderboard UI in place

--- lib/models/leaderboard.dart
+++ lib/models/leaderboard.dart
@@ -1,1 +1,26 @@
-class Leaderboard {}
+class Leaderboard {
+  int rank;
+  String name;
+  int total;
+  int nRL;
+  int aFL;
+  int numRoundsWon;
+  int aflMargins;
+  int aflUPS;
+  int nrlMargins;
+  int nrlUPS;
+
+  //constructor
+  Leaderboard({
+    required this.rank,
+    required this.name,
+    required this.total,
+    required this.nRL,
+    required this.aFL,
+    required this.numRoundsWon,
+    required this.aflMargins,
+    required this.aflUPS,
+    required this.nrlMargins,
+    required this.nrlUPS,
+  });
+}
