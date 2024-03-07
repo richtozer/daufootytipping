91b69220d96f2ace185bf056dd4a21a18b89f65c
richtozer <richard.tozer@gmail.com>
2024-03-07 19:56:22 +1030

    Chnage viewmodel init to allow dfor provider to work. Clean up UI elements

--- lib/pages/user_home/user_home_tips_gamelistitem.dart
+++ lib/pages/user_home/user_home_tips_gamelistitem.dart
@@ -45,17 +45,11 @@ class _GameListItemState extends State<GameListItem> {
 
   @override
   Widget build(BuildContext context) {
-    // return FutureBuilder(
-    //     future: gameTipsViewModel.initialLoadCompleted,
-    //     builder: (BuildContext context, AsyncSnapshot snapshot) {
-    //       if (!snapshot.hasData) {
-    //         return const Center(child: CircularProgressIndicator());
-    //       } else {
     return ChangeNotifierProvider<GameTipsViewModel>.value(
-        value: gameTipsViewModel,
-        child: Consumer<GameTipsViewModel>(
-            builder: (context, gameTipsViewModelConsumer, child) {
-          return Card(
+      value: gameTipsViewModel,
+      child: Consumer<GameTipsViewModel>(
+        builder: (context, gameTipsViewModelConsumer, child) {
+          Widget card = Card(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(8.0),
             ),
@@ -100,8 +94,7 @@ class _GameListItemState extends State<GameListItem> {
                       ),
                       Text(
                           style: const TextStyle(
-                            fontSize:
-                                16.0, // Adjust this value to make the text bigger or smaller
+                            fontSize: 16.0,
                           ),
                           textAlign: TextAlign.left,
                           widget.game.awayTeam.name),
@@ -125,45 +118,131 @@ class _GameListItemState extends State<GameListItem> {
                       items: carouselItems(gameTipsViewModelConsumer),
                       carouselController: gameTipsViewModelConsumer.controller,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: carouselItems(gameTipsViewModelConsumer)
-                          .asMap()
-                          .entries
-                          .map((entry) {
-                        return GestureDetector(
-                          onTap: () {
-                            gameTipsViewModelConsumer.controller!
-                                .animateToPage(entry.key);
-                          },
-                          child: Container(
-                            width: 6.0,
-                            height: 6.0,
-                            margin: const EdgeInsets.symmetric(
-                                vertical: 2.0, horizontal: 2.0),
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: (Theme.of(context).brightness ==
-                                            Brightness.dark
-                                        ? Colors.white
-                                        : Colors.black)
-                                    .withOpacity(gameTipsViewModelConsumer
-                                                .currentIndex ==
-                                            entry.key
-                                        ? 0.9
-                                        : 0.4)),
-                          ),
-                        );
-                      }).toList(),
-                    ),
                   ],
                 ),
               )
             ]),
           );
+
+          if (widget.game.gameState == GameState.notStarted) {
+            return card;
+          }
+
+          String bannerMessage;
+          Color bannerColor;
+
+          switch (widget.game.gameState) {
+            case GameState.resultNotKnown:
+              bannerMessage = "Live";
+              bannerColor = Color(0xffe21e31);
+              break;
+            case GameState.resultKnown:
+              bannerMessage = "Ended";
+              bannerColor = Colors.grey;
+              break;
+            case GameState.notStarted:
+              bannerMessage = "Not Started";
+              bannerColor = Colors.grey;
+              break;
+          }
+
+          return Banner(
+            color: bannerColor,
+            location: BannerLocation.topEnd,
+            message: bannerMessage,
+            child: card,
+          );
+        },
+      ),
+    );
+  }
+
+  Widget build2(BuildContext context) {
+    return ChangeNotifierProvider<GameTipsViewModel>.value(
+        value: gameTipsViewModel,
+        child: Consumer<GameTipsViewModel>(
+            builder: (context, gameTipsViewModelConsumer, child) {
+          return Banner(
+            color: Colors.white54,
+            location: BannerLocation.topEnd,
+            message: "Ended",
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              color: Colors.grey[300],
+              child: Row(children: [
+                Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: SizedBox(
+                    width: 100,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.game.homeTeam.name,
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(
+                            fontSize:
+                                16.0, // Adjust this value to make the text bigger or smaller
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(
+                              widget.game.homeTeam.logoURI ??
+                                  (widget.game.league == League.nrl
+                                      ? League.nrl.logo
+                                      : League.afl.logo),
+                              width: 20,
+                              height: 20,
+                            ),
+                            const Text(textAlign: TextAlign.left, ' V '),
+                            SvgPicture.asset(
+                              widget.game.awayTeam.logoURI ??
+                                  (widget.game.league == League.nrl
+                                      ? League.nrl.logo
+                                      : League.afl.logo),
+                              width: 20,
+                              height: 20,
+                            ),
+                          ],
+                        ),
+                        Text(
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                            ),
+                            textAlign: TextAlign.left,
+                            widget.game.awayTeam.name),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      CarouselSlider(
+                        options: CarouselOptions(
+                            height: 120,
+                            enlargeFactor: 1.0,
+                            enlargeCenterPage: true,
+                            enlargeStrategy: CenterPageEnlargeStrategy.zoom,
+                            enableInfiniteScroll: false,
+                            onPageChanged: (index, reason) {
+                              gameTipsViewModelConsumer.currentIndex = index;
+                            }),
+                        items: carouselItems(gameTipsViewModelConsumer),
+                        carouselController:
+                            gameTipsViewModelConsumer.controller,
+                      ),
+                    ],
+                  ),
+                )
+              ]),
+            ),
+          );
         }));
-    //   }
-    // });
   }
 
   List<Widget> carouselItems(GameTipsViewModel gameTipsViewModel) {
@@ -188,7 +267,8 @@ class _GameListItemState extends State<GameListItem> {
       future: gameTipsViewModel.gettip(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return LiveScoring(tipGame: snapshot.data!);
+          return LiveScoring(
+              tipGame: snapshot.data!, gameTipsViewModel: gameTipsViewModel);
         } else if (snapshot.hasError) {
           return Text('Error: ${snapshot.error}');
         } else {
