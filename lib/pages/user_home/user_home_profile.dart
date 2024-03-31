54664050f4d5aa9568faf13ff02e46214cd2dd2e
richtozer <richard.tozer@gmail.com>
2024-04-01 07:05:45 +1030

    Update dependencies and remove unused import

--- lib/pages/user_home/user_home_profile.dart
+++ lib/pages/user_home/user_home_profile.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:daufootytipping/models/daucomp.dart';
 import 'package:daufootytipping/models/tipper.dart';
 import 'package:daufootytipping/models/tipperrole.dart';
