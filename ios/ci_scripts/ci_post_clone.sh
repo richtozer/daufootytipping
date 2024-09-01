1421210618b2635948d1af9ec7a8ade6c85bc52d
richtozer <richard.tozer@gmail.com>
2024-09-02 07:15:05 +1000

    fix xcloud build issues

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 # The default execution directory of this script is the ci_scripts directory.
 cd CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
