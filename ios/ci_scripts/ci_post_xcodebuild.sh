dfe36c49e2264a809a252f65e3d2e640542778cb
richtozer <richard.tozer@gmail.com>
2025-02-08 08:32:05 +1100

    github tag build numberintegration

---
+++ ios/ci_scripts/ci_post_xcodebuild.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+# 1. Create 'ci_scripts' folder in your main project directory
+# 2. Create 'ci_post_xcodebuild.sh' inside of it
+# 3. Make it an executable by running 'chmod +x $ci_post_xcodebuild.sh'
+
+set -e # fails build if any command fails
+
+if [ ${CI_XCODEBUILD_EXIT_CODE} != 0 ]
+then
+    exit 1
+fi
+
+if [[ -n $CI_APP_STORE_SIGNED_APP_PATH ]]; # checks if there is an AppStore signed archive after running xcodebuild
+then
+    BUILD_TAG=${CI_BUILD_NUMBER}
+    VERSION=$(cat ../${CI_PRODUCT}.xcodeproj/project.pbxproj | grep -m1 'MARKETING_VERSION' | cut -d'=' -f2 | tr -d ';' | tr -d ' ')
+
+    git tag ${CI_PRODUCT}/Release/$VERSION\($BUILD_TAG\)
+
+    git push --tags https://${GIT_AUTH}@github.com/richtozer/daufootytipping.git
+fi
+
+# use workflow Environment to configure your GIT_AUTH variable - username:personalAccessToken
+# see https://gist.github.com/Berhtulf/ab3fb663187e7644410c0401b207aa45 for more information
