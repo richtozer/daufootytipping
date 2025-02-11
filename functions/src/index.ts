f6151a9459e8126ac4ebf0ff12a00db398125b42
richtozer <richard.tozer@gmail.com>
2025-02-12 10:34:47 +1100

    Remove legacy tipping firebase function - not required

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,9 @@
 // This is the entry point for the Firebase Functions
-import tipperFunctions = require("./services/legacy_service");
 import msgFunctionNotTipped =
     require("./services/messaging_service_send_not_tipped_reminders");
 
 import * as admin from "firebase-admin";
 
-exports.submitLegacyTips = tipperFunctions.submitLegacyTips;
 exports.sendReminders = msgFunctionNotTipped.sendHourlyReminders;
 
 admin.initializeApp();
