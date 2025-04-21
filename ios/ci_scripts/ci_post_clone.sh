a601d908f90ad808c1efb818545d82d7eadd602d
richtozer <richard.tozer@gmail.com>
2025-04-21 03:44:57 +0100

    Refactor Flutter installation method in ci_post_clone.sh to use curl instead of git

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -6,9 +6,13 @@ set -e
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
-# Install Flutter using git.
-git clone https://github.com/flutter/flutter.git --branch stable
-export PATH="$PATH:$HOME/flutter/bin"
+## Install Flutter using git.
+#git clone https://github.com/flutter/flutter.git --branch stable
+#export PATH="$PATH:$HOME/flutter/bin"
+
+# Install Flutter using curl.
+curl -sLO "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_3.29.0-stable.zip"
+unzip -qq flutter_macos_3.29.0-stable.zip -d $HOME
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
 flutter precache --ios
