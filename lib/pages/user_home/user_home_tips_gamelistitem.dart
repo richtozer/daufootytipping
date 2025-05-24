0c87ed30a19a3d1e4c07b6bd9f083b33424b3ed8
richtozer <richard.tozer@gmail.com>
2025-05-24 15:19:42 +0100

    fix: remove corousel from league ladder ontap gesture

--- lib/pages/user_home/user_home_tips_gamelistitem.dart
+++ lib/pages/user_home/user_home_tips_gamelistitem.dart
@@ -203,141 +203,137 @@ class _GameListItemState extends State<GameListItem> {
           // Reference to the game for easier access in onTap
           final Game game = gameTipsViewModelConsumer.game;
 
-          Widget gameDetailsCard = GestureDetector(
-            // Wrapped Card with GestureDetector
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => LeagueLadderPage(
-                    league: game.league,
-                    teamDbKeysToDisplay: [
-                      game.homeTeam.dbkey,
-                      game.awayTeam.dbkey
-                    ],
-                    customTitle:
-                        "League Leaderboard comparison", // Updated title
-                  ),
-                ),
-              );
-            },
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              color: Colors.white70,
-              surfaceTintColor: League.nrl.colour,
-              child: Row(children: [
-                gameTipsViewModelConsumer.game.gameState ==
-                        GameState.startedResultNotKnown
-                    ? Tooltip(
-                        message: 'Click here to edit scoring for this game',
-                        child: GestureDetector(
-                          onTap: () => showMaterialModalBottomSheet(
-                              expand: false,
-                              context: context,
-                              builder: (context) => LiveScoringModal(
-                                  gameTipsViewModelConsumer.tip!)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(0.0),
-                            child: SizedBox(
-                              width: 130,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        gameTipsViewModelConsumer
-                                            .game.homeTeam.name,
-                                        textAlign: TextAlign.left,
+          Widget gameDetailsCard = Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            color: Colors.white70,
+            surfaceTintColor: League.nrl.colour,
+            child: Row(children: [
+              gameTipsViewModelConsumer.game.gameState ==
+                      GameState.startedResultNotKnown
+                  ? Tooltip(
+                      message: 'Click here to edit scoring for this game',
+                      child: GestureDetector(
+                        onTap: () => showMaterialModalBottomSheet(
+                            expand: false,
+                            context: context,
+                            builder: (context) => LiveScoringModal(
+                                gameTipsViewModelConsumer.tip!)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(0.0),
+                          child: SizedBox(
+                            width: 130,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      gameTipsViewModelConsumer
+                                          .game.homeTeam.name,
+                                      textAlign: TextAlign.left,
+                                      style: const TextStyle(
+                                        overflow: TextOverflow.ellipsis,
+                                        fontSize: 16.0,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    gameTipsViewModelConsumer.game.gameState ==
+                                            GameState.startedResultNotKnown
+                                        ? liveScoringHome(
+                                            gameTipsViewModelConsumer.game,
+                                            context)
+                                        : fixtureScoringHome(
+                                            gameTipsViewModelConsumer),
+                                  ],
+                                ),
+                                gameTipsViewModelConsumer.game.gameState ==
+                                        GameState.startedResultNotKnown
+                                    ? liveScoringEdit(context)
+                                    : Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          SvgPicture.asset(
+                                            gameTipsViewModelConsumer
+                                                    .game.homeTeam.logoURI ??
+                                                (gameTipsViewModelConsumer
+                                                            .game.league ==
+                                                        League.nrl
+                                                    ? League.nrl.logo
+                                                    : League.afl.logo),
+                                            width: 25,
+                                            height: 25,
+                                          ),
+                                          const Text(
+                                              textAlign: TextAlign.left, ' V '),
+                                          SvgPicture.asset(
+                                            gameTipsViewModelConsumer
+                                                    .game.awayTeam.logoURI ??
+                                                (gameTipsViewModelConsumer
+                                                            .game.league ==
+                                                        League.nrl
+                                                    ? League.nrl.logo
+                                                    : League.afl.logo),
+                                            width: 25,
+                                            height: 25,
+                                          ),
+                                        ],
+                                      ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
                                         style: const TextStyle(
                                           overflow: TextOverflow.ellipsis,
                                           fontSize: 16.0,
                                         ),
-                                      ),
-                                      SizedBox(
-                                        width: 5,
-                                      ),
-                                      gameTipsViewModelConsumer
-                                                  .game.gameState ==
-                                              GameState.startedResultNotKnown
-                                          ? liveScoringHome(
-                                              gameTipsViewModelConsumer.game,
-                                              context)
-                                          : fixtureScoringHome(
-                                              gameTipsViewModelConsumer),
-                                    ],
-                                  ),
-                                  gameTipsViewModelConsumer.game.gameState ==
-                                          GameState.startedResultNotKnown
-                                      ? liveScoringEdit(context)
-                                      : Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            SvgPicture.asset(
-                                              gameTipsViewModelConsumer
-                                                      .game.homeTeam.logoURI ??
-                                                  (gameTipsViewModelConsumer
-                                                              .game.league ==
-                                                          League.nrl
-                                                      ? League.nrl.logo
-                                                      : League.afl.logo),
-                                              width: 25,
-                                              height: 25,
-                                            ),
-                                            const Text(
-                                                textAlign: TextAlign.left,
-                                                ' V '),
-                                            SvgPicture.asset(
-                                              gameTipsViewModelConsumer
-                                                      .game.awayTeam.logoURI ??
-                                                  (gameTipsViewModelConsumer
-                                                              .game.league ==
-                                                          League.nrl
-                                                      ? League.nrl.logo
-                                                      : League.afl.logo),
-                                              width: 25,
-                                              height: 25,
-                                            ),
-                                          ],
-                                        ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                          style: const TextStyle(
-                                            overflow: TextOverflow.ellipsis,
-                                            fontSize: 16.0,
-                                          ),
-                                          textAlign: TextAlign.left,
-                                          gameTipsViewModelConsumer
-                                              .game.awayTeam.name),
-                                      SizedBox(
-                                        width: 5,
-                                      ),
-                                      gameTipsViewModelConsumer
-                                                  .game.gameState ==
-                                              GameState.startedResultNotKnown
-                                          ? liveScoringAway(
-                                              gameTipsViewModelConsumer.game,
-                                              context)
-                                          : fixtureScoringAway(
-                                              gameTipsViewModelConsumer),
-                                    ],
-                                  ),
-                                ],
-                              ),
+                                        textAlign: TextAlign.left,
+                                        gameTipsViewModelConsumer
+                                            .game.awayTeam.name),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    gameTipsViewModelConsumer.game.gameState ==
+                                            GameState.startedResultNotKnown
+                                        ? liveScoringAway(
+                                            gameTipsViewModelConsumer.game,
+                                            context)
+                                        : fixtureScoringAway(
+                                            gameTipsViewModelConsumer),
+                                  ],
+                                ),
+                              ],
                             ),
                           ),
                         ),
-                      )
-                    : Padding(
-                        padding: const EdgeInsets.all(0.0),
-                        child: SizedBox(
-                          width: 130,
+                      ),
+                    )
+                  : Padding(
+                      padding: const EdgeInsets.all(0.0),
+                      child: SizedBox(
+                        width: 130,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => LeagueLadderPage(
+                                  league: game.league,
+                                  teamDbKeysToDisplay: [
+                                    game.homeTeam.dbkey,
+                                    game.awayTeam.dbkey
+                                  ],
+                                  customTitle:
+                                      "League Leaderboard comparison", // Updated title
+                                ),
+                              ),
+                            );
+                          },
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.start,
                             children: [
@@ -477,27 +473,26 @@ class _GameListItemState extends State<GameListItem> {
                           ),
                         ),
                       ),
-                Expanded(
-                  child: Column(
-                    children: [
-                      CarouselSlider(
-                        options: CarouselOptions(
-                          height: 120,
-                          enlargeFactor: 1.0,
-                          enlargeCenterPage: true,
-                          enlargeStrategy: CenterPageEnlargeStrategy.zoom,
-                          enableInfiniteScroll: false,
-                        ),
-                        items: carouselItems(gameTipsViewModelConsumer,
-                            widget.isPercentStatsPage),
-                        carouselController:
-                            gameTipsViewModelConsumer.controller,
+                    ),
+              Expanded(
+                child: Column(
+                  children: [
+                    CarouselSlider(
+                      options: CarouselOptions(
+                        height: 120,
+                        enlargeFactor: 1.0,
+                        enlargeCenterPage: true,
+                        enlargeStrategy: CenterPageEnlargeStrategy.zoom,
+                        enableInfiniteScroll: false,
                       ),
-                    ],
-                  ),
-                )
-              ]),
-            ),
+                      items: carouselItems(
+                          gameTipsViewModelConsumer, widget.isPercentStatsPage),
+                      carouselController: gameTipsViewModelConsumer.controller,
+                    ),
+                  ],
+                ),
+              )
+            ]),
           );
 
           // if game is more than 3 hours in the past, don't show any banner
