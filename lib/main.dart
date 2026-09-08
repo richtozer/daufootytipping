8e90843432e588863dc6460a794b3cde2e757a0b
richtozer <richard.tozer@gmail.com>
2026-09-08 05:56:12 +0200

    fix(stats): restore DataTable2 rendering

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:material_ui/material_ui.dart'
+import 'package:flutter_localizations/flutter_localizations.dart'
     show GlobalMaterialLocalizations;
 import 'package:provider/provider.dart';
 import 'package:package_info_plus/package_info_plus.dart';
